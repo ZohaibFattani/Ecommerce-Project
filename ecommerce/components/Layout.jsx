ada8ec465d0d60bd362fa9ca8ddb9b7b04e8aab3
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-10 00:30:33 -0700

    added more products with sanity

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Head from "next/head";
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="layout">
+      <Head>
+        <title>Zohaib's Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        EMPTY
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
