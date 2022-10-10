4344689211593d761dafb743e1a6101e85fb7a2a
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-09 22:11:52 -0700

    imported styles

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -3,18 +3,15 @@ import React from "react";
 const Home = () => {
   return (
     <>
-    HeroBanner
-    <div>
-      <h2>Best Selling Products</h2>
-      <p>Speakers of many variations</p>
-    </div>
-    <div>
-    {["Product 1", "Product 2"].map( (product) => product)}
-    </div>
-
-    Footer
+      HeroBanner
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      Footer
     </>
-  )
+  );
 };
 
 export default Home;
