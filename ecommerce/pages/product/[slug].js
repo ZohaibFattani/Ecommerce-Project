afe2500d19c7057a0bc5dbd1fcf25e0f3b95461d
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-11 01:49:53 -0700

    implemented recomemnded products logic

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -8,6 +8,7 @@ import {
 } from "react-icons/ai";
 
 import { client, urlFor } from "../../lib/client";
+import { Product } from "../../components";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
@@ -25,7 +26,7 @@ const ProductDetails = ({ product, products }) => {
             ))}
           </div> */}
         </div>
-        <div className="product-details-desc">
+        <div className="product-detail-desc">
           <h1>{name}</h1>
           <div className="reviews">
             <div>
@@ -61,6 +62,16 @@ const ProductDetails = ({ product, products }) => {
             <button type="button" className="buy-now" onClick="">
               Buy Now
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>
