eb35e5c9ff7a83428350d05c0b768dd4e3e06df4
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-15 14:31:26 -0700

    implemented cart logix

--- ecommerce/components/Cart.jsx
+++ ecommerce/components/Cart.jsx
@@ -89,7 +89,7 @@ const Cart = () => {
                         </span>
                       </p>
                     </div>
-                    <button type="button" className="remove-item" onClick="">
+                    <button type="button" className="remove-item" onClick={()=> onRemove(item)}>
                       <TiDeleteOutline />
                     </button>
                   </div>
